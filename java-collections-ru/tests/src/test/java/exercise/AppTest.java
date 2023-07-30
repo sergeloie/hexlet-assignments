679c23a245d300ab8a91fd8524f886cb1662c4f5
serge loie <serge.loie@yandex.ru>
2023-07-30 22:08:45 +0500

    написан прототип теста

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -4,14 +4,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
+    // BEGIN
 
-    @Test
-    void testTake() {
-        // BEGIN
-        
+    private int n;
+    List<Integer> testList = new ArrayList<>();
+
+
+    @BeforeEach
+    void prepareTest() {
+        testList.add(0);
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        testList.add(5);
+        testList.add(6);
+        testList.add(7);
+    }
+
+
+        @Test
+        void testTake() {
+        n = 3;
+        List<Integer> compareList = new ArrayList<>();
+        compareList.add(0);
+        compareList.add(1);
+        compareList.add(2);
+        assertThat(compareList).isEqualTo(App.take(testList, n));
+
         // END
     }
 }

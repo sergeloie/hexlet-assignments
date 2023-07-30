13cfed2861789d5a4185ed922f8648bb37cf32de
serge loie <serge.loie@yandex.ru>
2023-07-30 21:42:20 +0500

    push test homework

---
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,17 @@
+package exercise;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testTake() {
+        // BEGIN
+        
+        // END
+    }
+}

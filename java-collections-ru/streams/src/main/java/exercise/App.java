275bae8f241a191423c41889a2116da3b46d65a5
serge.loie.voxys <serge.loie@yandex.ru>
2023-07-31 12:13:46 +0500

    realize method getCountOfFreeEmails

---
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import java.util.List;
+import java.util.Arrays;
+
+// BEGIN
+public class App {
+
+    public static int getCountOfFreeEmails(List<String> emailsList) {
+        return (int) emailsList.stream()
+                .filter(email -> email.endsWith("gmail.com") || email.endsWith("yandex.ru") || email.endsWith("hotmail.com"))
+                .count();
+    }
+}
+// END

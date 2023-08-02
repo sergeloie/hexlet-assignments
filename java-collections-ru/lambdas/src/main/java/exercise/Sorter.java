4d5625f68ac4fdc3a1459ade305e977a04c5e33d
serge.loie.voxys <serge.loie@yandex.ru>
2023-08-02 16:53:01 +0500

    сорировка Компаратором

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,5 +1,6 @@
 package exercise;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,7 +11,7 @@ public class Sorter {
     public static List<String> takeOldestMans(List<Map<String, String>> listPeople) {
         return listPeople.stream()
                 .filter(map -> map.get("gender").equals("male"))
-                .sorted((map1, map2) -> map1.get("birthday").compareTo(map2.get("birthday")))
+                .sorted(Comparator.comparing(map -> map.get("birthday")))
                 .map(map -> map.get("name"))
                 .collect(Collectors.toList());
     }
